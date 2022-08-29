2d7c950147b7e37195ede1996fe961d8553f5a48
BogKowit <BogKowit@gmail.com>
2022-08-29 10:04:14 +0200

    ++

--- src/components/common/Results/Results.tsx
+++ src/components/common/Results/Results.tsx
@@ -2,7 +2,7 @@ import "./Result.css";
 import { IoRemoveCircleOutline, IoPencil } from "react-icons/io5";
 import { useAPI } from "../../../context/contextFetch";
 import { UseUI } from "../../../context/context";
-import { PropsDataStatics } from "../../../utils/types"
+import { PropsDataStatics } from "../../../utils/types";
 
 const Results = () => {
   const { fetchData, deleteItemFromData, getDataFromServer, setModalById } =
@@ -33,12 +33,7 @@ const Results = () => {
           const { id, name, number } = value;
           return (
             <li key={id} className="listitem ">
-              <p
-                className=" column firstColumn"
-                onClick={() => console.log({ name })}
-              >
-                {name}
-              </p>
+              <p className=" column firstColumn">{name}</p>
               <p className=" column middleColumn">{number}</p>
               <div className="column iconColumn lastColumn ">
                 <button
